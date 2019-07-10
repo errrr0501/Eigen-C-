a41c83bf3a4f0023b2f4263fe00cdc987c1eff42
errrr0501 <errrr0501done@gmail.com>
2019-07-10 11:08:16 +0800

    add random find maxcoeff and mincoeff

---
+++ mat_04.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <eigen3/Dense>
+using namespace std;
+using namespace Eigen;
+int main(){
+    
+  Matrix3f m = Matrix3f::Random();
+  std::ptrdiff_t i, j;
+  float minOfM = m.minCoeff(&i,&j);
+  cout << "Here is the matrix m:\n" << m << endl;
+  cout << "Its minimum coefficient (" << minOfM 
+       << ") is at position (" << i << "," << j << ")\n\n";
+  RowVector4i v = RowVector4i::Random();
+  int maxOfV = v.maxCoeff(&i);
+  cout << "Here is the vector v: " << v << endl;
+  cout << "Its maximum coefficient (" << maxOfV 
+       << ") is at position " << i << endl;
+       
+        }
